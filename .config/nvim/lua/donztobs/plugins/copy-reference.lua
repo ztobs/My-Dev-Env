a4497af6467f14b605b46b56eb0598f87dbcde37
Oluwatobi Lukan <lukan.oluwatobi@impulse.de>
2026-02-17 20:51:50 +0100

    fix: copy reference not working

--- .config/nvim/lua/donztobs/plugins/copy-reference.lua
+++ .config/nvim/lua/donztobs/plugins/copy-reference.lua
@@ -1,12 +1,29 @@
 -- Using lazy.nvim
 return {
   "cajames/copy-reference.nvim",
-  opts = {
-    register = "+", -- System clipboard
-    use_git_root = true, -- Use git root for relative paths in repos
+  lazy = false, -- Load immediately on startup
+  priority = 1000, -- Load early
+  dependencies = {
+    "folke/which-key.nvim",
   },
-  keys = {
-    { "yr", "<cmd>CopyReference file<cr>", mode = { "n", "v" }, desc = "Copy file path" },
-    { "yrr", "<cmd>CopyReference line<cr>", mode = { "n", "v" }, desc = "Copy file:line reference" },
-  },
+  config = function()
+    require("copy-reference").setup({
+      register = "+", -- System clipboard
+      use_git_root = true, -- Use git root for relative paths in repos
+    })
+    
+    -- Set up keymaps manually after plugin loads
+    vim.keymap.set({ "n", "v" }, "yr", "<cmd>CopyReference file<cr>", { desc = "Copy file path" })
+    vim.keymap.set({ "n", "v" }, "yrr", "<cmd>CopyReference line<cr>", { desc = "Copy file:line reference" })
+    
+    -- Register with which-key
+    local wk_ok, wk = pcall(require, "which-key")
+    if wk_ok then
+      wk.add({
+        { "yr", group = "yank reference" },
+        { "yr", "<cmd>CopyReference file<cr>", desc = "Copy file path", mode = { "n", "v" } },
+        { "yrr", "<cmd>CopyReference line<cr>", desc = "Copy file:line reference", mode = { "n", "v" } },
+      })
+    end
+  end,
 }
